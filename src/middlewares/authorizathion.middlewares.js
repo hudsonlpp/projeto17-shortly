f280894841716123a15a931f7d46ef78dc7ac876
Hudson Peres <hudsonluizperes@poli.ufrj.br>
2023-03-03 17:08:38 -0300

    shortens

--- src/middlewares/authorizathion.middlewares.js
+++ src/middlewares/authorizathion.middlewares.js
@@ -5,20 +5,23 @@ export async function authValidation(req, res, next) {
     const token = authorization?.replace("Bearer ", "");
 
     if(!token) return res.sendStatus(401);
-
     try{
-        const { rows } = await db.query(
+        const { rows:sessions } = await db.query(
             'SELECT * FROM sessions WHERE token = $1',
             [token]
         );
-    
-        const [session] = rows;
-        if (!session) return res.sendStatus(401);
+        console.log(sessions)
+        const [session] = sessions;
+    if (!session) return res.sendStatus(401);
 
         const {rows: users } = await db.query(
             'SELECT * FROM users WHERE id = $1',
             [session.userId]
         );
+        const [user] = users;
+        if(!user) return res.sendStatus(401)
+        res.locals.user = user;
+        next();
     } catch (err) {
         res.status(500).send(err.message);
     }
--- src/middlewares/schemaValidation.middlewares.js
+++ src/middlewares/schemaValidation.middlewares.js
@@ -3,7 +3,7 @@ export function validateSchema(schema) {
         const { error } = schema.validate(req.body, { abortEarly: false });
         if(error){
             return res.status(422).send(error.details.map((detail) => detail.message));
-            next();
         }
+        next();
     }
 }
